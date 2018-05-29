95e58c362cd94948ccd26769517e50c660e7a96d
SYS0xWord <miguelbatz2@gmail.com>
2018-05-29 16:06:06 -0600

    Interfaz java

---
+++ Inventario2./src/inventario2/Conexion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario2;
+
+/**
+ *
+ * @author sys515
+ */
+import java.sql.*;
+import com.mysql.jdbc.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sys515
+ */
+public class Conexion {
+
+    Connection cn;
+    Statement st;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost/InventarioPEPS/UEPS", "root", "InventarioPEPS");
+      
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("no conecta");
+        }
+        return cn;
+    }
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("No soportado");
+    }
+}
